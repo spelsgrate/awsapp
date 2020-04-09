e0bcbc8e04298b407a2536ef95bc4acc03c7ee50
Ubuntu <ubuntu@ip-172-31-85-45.ec2.internal>
2020-04-09 17:11:25 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+    import React, { Component } from 'react';
+    import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // We import our Home component here...
+    import Home from './Home';
+    import './App.css';
 
-export default App;
+    class App extends Component {
+      render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            {/* Here we add our Home component to be render it */}
+            <Home />
+          </div>
+        );
+      }
+    }
+
+    export default App;
