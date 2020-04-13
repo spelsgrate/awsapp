15ae586dc840e6de50aa8f543839c8cf30eb2c6c
Nate Martin <natebemartin@gmail.com>
2020-04-13 20:30:33 +0000

    finished Todo, begin Pomodoo

---
+++ src/components/Pomodoro/Timer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Timer.css';
+
+class Timer extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="Pomodoro">
+
+      </div>
+    );
+  }
+}
+
+export default Timer;
