2cae69140a5deafc0337b65d922267f2638f55b3
Nate Martin <natebemartin@gmail.com>
2020-04-15 04:46:35 +0000

    es6 conversion, folder deletion

--- server/express-app/app.js
+++ server/express-app/app.js
@@ -1,42 +1,20 @@
-  import createError from 'http-errors';
+  // Dependencies
   import express from 'express';
   import path from 'path';
-  import cookieParser from 'cookie-parser';
-  import logger from 'morgan';
 
-  import indexRouter from './routes/index';
-  import usersRouter from './routes/users';
+  // Controllers
+  import apiController from './controllers/api';
 
+  // Express Application
   const app = express();
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
-
-  app.use(logger('dev'));
+  // Middlewares
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use('/', indexRouter);
-  app.use('/users', usersRouter);
-
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    next(createError(404));
-  });
-
-  // error handler
-  app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+  // Routes
+  app.use('/api', apiController);
 
   // Listening port
   app.listen(3000);
+  
